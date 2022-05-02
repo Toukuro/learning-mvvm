25e7e9c8864955011e10c6978f3c7854497ffd8d
tokuro <kj_adachi@nifty.com>
2022-05-02 17:47:13 +0900

    WPF版の登録

---
+++ wpf/WpfSample/WpfLogin/Model/UserModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLogin.Model
+{
+    public class UserModel
+    {
+        private List<UserRecord> userList;
+
+        public UserModel()
+        {
+            userList = new List<UserRecord>
+            {
+                new UserRecord("梅田 太郎", "t_umeda", "umeda"),
+                new UserRecord("淀屋橋 一郎", "i_yodoyabashi", "yodoyabashi"),
+                new UserRecord("本町 次郎", "j_honmachi", "honmachi"),
+            };
+        }
+
+        public bool Authenticate(string loginId, string password)
+        {
+            return userList
+                .Any(user => user.LoginId == loginId && user.Password == password);
+        }
+
+        public bool IsLogined(string loginId)
+        {
+            return userList
+                .Any(user => user.LoginId == loginId && user.IsLogined);
+        }
+    }
+}
---
+++ wpf/WpfSample/WpfLogin/Model/UserRecord.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLogin.Model
+{
+    public class UserRecord
+    {
+        public string UserName { get;  }
+        public string LoginId { get; }
+        public string Password { get; }
+        public bool IsLogined { get; set; } = false;
+
+        public UserRecord(string userName, string loginId, string password)
+        {
+            UserName = userName;
+            LoginId = loginId;
+            Password = password;
+        }
+    }
+}
